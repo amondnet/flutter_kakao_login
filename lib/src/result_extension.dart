060c7c27ae90f0dc197a7ad7eab65a085f3da167
Minsu Lee <amond@amond.net>
2020-09-04 13:55:08 +0900

    feat: kakao v2

--- lib/src/result_extension.dart
+++ lib/src/result_extension.dart
@@ -2,11 +2,10 @@ import 'package:async/async.dart' as $async;
 import 'package:flutter_kakao_login/src/kakao_login_error.dart';
 import 'package:meta/meta.dart';
 
-extension ResultExtension on $async.Result {
+extension ResultExtension<T> on $async.Result<T> {
   @optionalTypeArgs
   R when<R extends Object>(R $default(),
-      {@required R success(dynamic value),
-      @required R fail(KakaoSdkError error)}) {
+      {@required R success(T value), @required R fail(KakaoSdkError error)}) {
     assert($default != null);
     assert(success != null);
     assert(fail != null);
